113ba134a04e8839a7ba0b61e6adabbaf18cbb78
Brynn Haley <brynn.haley@live.com>
2019-04-23 21:14:20 -0300

    First commit. Start of project.

---
+++ ParkingMetre.java
@@ -0,0 +1,76 @@
+/**
+ This class represents a parking metre for the purpose of calculating parking fees based on the metre's 
+ location, dollar rate per minute, and number of minutes a car has been parked. 
+ @author Brynn Haley
+*/
+
+public class ParkingMetre 
+{
+
+	/**
+	The street name the metre is located on.
+	*/
+	public String location; 
+	
+	
+	/**
+	The cost of parking at the metre (in $/minute).
+	*/
+	public double parkingRate = 0;
+	
+
+	/**
+	The amount of time on the parking metre (minutes).
+	*/
+	public double time = 0;
+
+	/**
+	This method constructs a parking metre in a specified location and parking rate. 
+	@param locationIn The location of the metre.
+	@param parkingRateIn The cost of parking at the metre (in $/minute).
+	*/
+	public void ParkingMetre(String location, double parkingRate)
+	{
+	this.location = location;
+	this.parkingRate = parkingRate;
+	}
+	
+	/**
+	This method retrieves the location of the parking metre.
+	@return The location of the metre.
+	*/
+	public String getLocation() {
+		return location;
+	}
+	
+	/**
+	This method retrieves the cost of parking at the metre.
+	@return The cost of parking at the metre ($/min).
+	*/
+	public double getParkingRate() {
+		return parkingRate;
+	}
+
+	/**
+	This method retrieves the amount of time left on the metre.
+	@return The amount of time on the metre.
+	*/
+	public double getTime() {
+		return time;
+	}
+	
+	/**
+	This method parks at a metre for a certain period of time.
+	@param time The time parked at the metre (in minutes).
+	*/
+	public void setTime(double timeIn) {
+		time += timeIn;
+	}
+	
+	/**
+	This method adds money to the metre.
+	@param moneyAdded Amount of money added to the metre.
+	*/
+	public void addMoney(double moneyAdded) {
+	}	
+} 
